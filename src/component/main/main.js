5843a6b8a7ae59f5e2005c328e696efea2e4d3c5
akat32 <baduk32127@gmail.com>
2019-04-23 15:55:48 +0900

    add Scene, Camera

--- src/component/main/main.js
+++ src/component/main/main.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 import * as THREE from 'three'
-const scene = new THREE.Scene()
 
 class Main extends Component {
+    componentDidMount () {
+        const width = this.mount.clientWidth
+        const height = this.mount.clientHeight
+
+        // ADD scene
+        this.scene = new THREE.Scene()
+
+        //ADD Camera
+        this.camera = new THREE.PerspectiveCamera(
+            75,
+            width / height,
+            0.1,
+            1000
+        )
+        this.camera.position.z = 4;
+    }
     render () {
         return (
             <div>
