6587defd5e3fcaa1a600637df8d368cc96571f22
akat32 <baduk32127@gmail.com>
2019-04-23 16:05:35 +0900

    add some code

--- src/component/main/main.js
+++ src/component/main/main.js
@@ -18,11 +18,44 @@ class Main extends Component {
             1000
         )
         this.camera.position.z = 4;
+        
+        //ADD RENDERER
+        this.renderer = new THREE.WebGLRenderer({ antialias: true })
+        this.renderer.setClearColor('#000000')
+        this.renderer.setSize(width, height)
+        this.mount.appendChild(this.renderer.domElement)
+         //ADD CUBE
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
+        this.cube = new THREE.Mesh(geometry, material)
+        this.scene.add(this.cube)
+        this.start()
+    }
+    componentWillUnmount(){
+        this.stop()
+        this.mount.removeChild(this.renderer.domElement)
+    }
+    start = () => {
+        if (!this.frameId) {
+          this.frameId = requestAnimationFrame(this.animate)
+        }
+    }
+    stop = () => {
+        cancelAnimationFrame(this.frameId)
+    }
+    animate = () => {
+        this.cube.rotation.x += 0.01
+        this.cube.rotation.y += 0.01
+        this.renderScene()
+        this.frameId = window.requestAnimationFrame(this.animate)
+    }
+    renderScene = () => {
+        this.renderer.render(this.scene, this.camera)
     }
     render () {
         return (
             <div>
-                에헤헤
+                asdasdsad
             </div>
         )
     }
